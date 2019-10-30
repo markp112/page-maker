bca9dcd7ac9796fc2e37c16771e740519b72cb46
Mark Phillips <mark.phillips1965@googlemail.com>
2019-10-30 09:12:46 +0000

    filtered fonts search added

--- src/app/components/font-drop-down/font-drop-down.component.ts
+++ src/app/components/font-drop-down/font-drop-down.component.ts
@@ -5,35 +5,45 @@ import * as WebFont from "webfontloader";
 
 
 @Component({
-  selector: 'app-font-drop-down',
-  templateUrl: './font-drop-down.component.html',
-  styleUrls: ['./font-drop-down.component.scss']
+  selector: "app-font-drop-down",
+  templateUrl: "./font-drop-down.component.html",
+  styleUrls: ["./font-drop-down.component.scss"]
 })
 export class FontDropDownComponent implements OnInit {
+  googleFont: IGooglefont = googleFont;
+  fontFamilies: string[] = [];
+  fontsFiltered: IGooglefont;
 
+  @Output() handleFontSelected: EventEmitter<string> = new EventEmitter();
 
-  googleFont:IGooglefont = googleFont;
-  fontFamilies:string[] =[];
-
-  @Output() handleFontSelected:EventEmitter<string> = new EventEmitter();
-
-  handleListItemClick(fontName){
+  handleListItemClick(fontName) {
     this.handleFontSelected.emit(fontName);
   }
 
-  constructor() { }
+  handleSearchInput(searchValue: string) {
+    console.log(this.fontsFiltered.items);
+    console.log(this.googleFont.items);
+    const search = searchValue.toLowerCase();
+  
+    this.fontsFiltered.items = this.googleFont.items.filter(font => {
+      return font.family
+        .toLowerCase()
+        .replace(" ", "-")
+        .includes(search);
+    });   
+  }
+
+  constructor() {}
 
   ngOnInit() {
-    googleFont.items.forEach(font =>{
+    googleFont.items.forEach(font => {
       this.fontFamilies.push(font.family.toString());
-    })
-
+    });
+    this.fontsFiltered = this.googleFont;
     WebFont.load({
-
       google: {
         families: this.fontFamilies
       }
     });
   }
-
 }
---
+++ src/app/shared/font-service.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { IGooglefont } from "src/app/models/interfaces/google-font-api";
+import { googleFont } from "src/assets/data/mock/google-font";
+import * as WebFont from "webfontloader";
+
+@Injectable({
+  providedIn: "root"
+})
+export class FontServiceService {
+  googleFont: IGooglefont = googleFont;
+  fontFamilies: string[] = [];
+  fontsFiltered: IGooglefont;
+
+  getFonts(filter:string):IGooglefont {
+    if(!filter) { return this.fontsFiltered}
+  
+      const search = filter.toLowerCase().trim();
+      this.fontsFiltered.items = this.googleFont.items.filter(font => {
+        return font.family
+          .toLowerCase()
+          .replace(" ", "-")
+          .includes(search);
+      });  
+    
+  }
+  constructor() {
+    googleFont.items.forEach(font => {
+      this.fontFamilies.push(font.family.toString());
+    });
+    
+    this.fontsFiltered = this.googleFont;
+    WebFont.load({
+      google: {
+        families: this.fontFamilies
+      }
+    });
+  }
+}
